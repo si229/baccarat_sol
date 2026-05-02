a136ea9adbcaf2439233a0f3cad8813702cf3c03
c <se@c.com>
2026-05-02 10:12:34 +0800

    a

--- contracts/Baccarat.sol
+++ contracts/Baccarat.sol
@@ -1,136 +1,157 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.20;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/**
- * @title Owner
- * @dev Set & change owner
- */
+
 contract Baccarat {
 
-    address private owner;
-    mapping(address=>uint256) private playerBalances;
-    mapping(address=>bool) private betState;
+    address private _owner;
+    mapping(address => mapping(address => int256)) internal _balance;
+    mapping(address => mapping(address => bool)) internal _betState;
     bytes32 public resultSha256;
     uint64 public roundId;
-    uint256 private prizePool;
+    mapping(address=>int256) private _prizePool;
 
-
-    event Deposit(address player, uint256 amount,uint256 balance);
-    event Withdraw(address player, uint256 amount,uint256 contractBalance ,uint256 walletBalance);
-    event Settle(address player, int256 amount,uint256 balance);
+    event Deposit(address indexed player,address indexed token, uint256 amount,int256 balance);
+    event Withdraw(address indexed player,address indexed token,  uint256 amount,int256 balance);
+    event Settle(address indexed player, address indexed token,int256 amount,int256 balance);
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Caller is not owner");
+        require(msg.sender == _owner, "Caller is not owner");
         _;
     }
 
     constructor() {
-        owner = msg.sender; 
+        _owner = msg.sender; 
     }
 
     function setRoundId(uint64 ID) external   {
         roundId = ID;
     }
 
-    function bet() external {
-        if (playerBalances[msg.sender] != 0){
-            betState[msg.sender] = true;
+    function bet(address token) external {
+        if (_balance[msg.sender][token] != 0){
+            _betState[msg.sender][token] = true;
         }
     }
 
     function transferOwnership(address newOwner) external onlyOwner {
-        owner = newOwner;
+        _owner = newOwner;
     }
 
     function isOwner()  external view returns(bool) {
-        return msg.sender == owner;
+        return msg.sender == _owner;
     }
 
-    function checkBetValid(address[] memory roles) external view onlyOwner returns (bool[] memory){
-        bool[] memory check = new bool[](roles.length);
-        for(uint i=0;i<roles.length;i++){
-            check[i] = betState[roles[i]];
-        }
-        return check;
+    function getPrizePool(address token) external view onlyOwner returns (int256) {
+        
+        return _prizePool[token];
     }
 
-    function setResultHash(bytes32 result) external  {
-        resultSha256 = result;
+    function getBalance() external view returns (int256){
+        return _balance[msg.sender][address(0)];
     }
 
-    function getPrizePool() external view onlyOwner returns (uint256) {
-        return prizePool;
-    }
-
-    function getBalance() external view returns (uint256){
-        return playerBalances[msg.sender];
+    function getBalance(address token) external view returns (int256){
+        return _balance[msg.sender][token];
     }
 
     function depositPrizePool() external payable {
-            prizePool+=msg.value;
+            _prizePool[address(0)]+=int256(msg.value);
     }
 
    
     function withdrawPrizePool(uint256 amount) external onlyOwner {
-        require(prizePool >= amount, "Insufficient balance");
-        prizePool -= amount;
-        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(_prizePool[address(0)] >= int256(amount), "Insufficient balance");
+        _prizePool[address(0)] -= int256(amount);
+        (bool success, ) = payable(msg.sender).call{value: uint256(int256(amount))}("");
+        require(success, "Transfer failed");
+    }
+
+
+    function withdrawPrizePool(address token, uint256 amount) external onlyOwner {
+        require(amount > 0, "invalid amount");
+        require(_prizePool[token] >= int256(amount), "Insufficient balance");
+        _prizePool[token] -= int256(amount);
+        bool success = IERC20(token).transfer(msg.sender, amount);
         require(success, "Transfer failed");
     }
 
     function deposit() external payable {
         require(0 < msg.value, "AMOUNT must be greater than 0 ");
-        playerBalances[msg.sender]+=msg.value;
-        emit Deposit(msg.sender,msg.value,playerBalances[msg.sender]);
-        
+        _balance[msg.sender][address(0)] += int256(msg.value);
+        emit Deposit(msg.sender,address(0) ,msg.value,_balance[msg.sender][address(0)]);
         }
     
+
+    function deposit(address token, uint amount) external {
+        require(amount > 0, "zero amount");
+
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        require(success, "transfer failed");
+        _balance[msg.sender][token] += int256(amount);
+        emit Deposit(msg.sender,token, amount,_balance[msg.sender][token]);
+    }
+
     function withdraw(uint256 amount) external payable {
-            require(playerBalances[msg.sender] >= amount, "Insufficient balance");
+            require(_balance[msg.sender][address(0)] >= int256(amount), "Insufficient balance");
             require(0 < amount, "AMOUNT must be greater than 0 ");
-            require(betState[msg.sender] == true, "Please settle first before proceeding");
-            playerBalances[msg.sender]-=amount;
+            require(_betState[msg.sender][address(0)] == true, "Please settle first before proceeding");
+            _balance[msg.sender][address(0)] -= int256(amount);
             (bool success, ) = payable(msg.sender).call{value: amount}("");
             require(success, "Transfer failed");
-            emit Withdraw(msg.sender,amount,playerBalances[msg.sender],msg.sender.balance);
+            emit Withdraw(msg.sender,address(0) ,amount,_balance[msg.sender][address(0)]);
+    }
+
+    function withdraw(address token, uint256 amount) external payable {
+            require(_balance[msg.sender][token] >= int256(amount), "Insufficient balance");
+            require(0 < amount, "AMOUNT must be greater than 0 ");
+            require(_betState[msg.sender][token] == true, "Please settle first before proceeding");
+            _balance[msg.sender][token] -= int256(amount);
+            bool success = IERC20(token).transfer(msg.sender, amount);
+            require(success, "Transfer failed");
+            emit Withdraw(msg.sender,token, amount,_balance[msg.sender][token]);
     }
 
 
    function settle(address player,uint256 SettleAmount,bool isLoss ) external onlyOwner {
         if (isLoss){
-            if (playerBalances[player]<=SettleAmount){
-                delete playerBalances[player];
+            if (_balance[player][address(0)] <= int256(SettleAmount)){
+                delete _balance[player][address(0)];
             }else{
-                playerBalances[player] -= SettleAmount;
+                _balance[player][address(0)] -= int256(SettleAmount);
             }
-            prizePool+=SettleAmount;
+            _prizePool[address(0)]+=int256(SettleAmount);
         }else{
-          playerBalances[player] += SettleAmount;
-          if (prizePool<=SettleAmount){
-            prizePool = 0;
+          _balance[player][address(0)] += int256(SettleAmount);
+          if (_prizePool[address(0)]<=int256(SettleAmount)){
+            _prizePool[address(0)] = 0;
           }else{
-            prizePool-=SettleAmount;
+            _prizePool[address(0)]-=int256(SettleAmount);
           }
 
         }
 
-        betState[player] = true;
+        _betState[player][address(0)]   = true;
        
     }
 
 
     receive() external payable { 
-        prizePool+=msg.value;
-        emit  Deposit(msg.sender, msg.value,prizePool);
+        _prizePool[address(0)]+=int256(msg.value);
+        emit  Deposit(msg.sender, address(0), msg.value,_prizePool[address(0)]);
 
     }
 
     fallback() external payable {
-        prizePool+=msg.value;
-        emit  Deposit(msg.sender, msg.value,prizePool);
+        _prizePool[address(0)]+=int256(msg.value);
+        emit  Deposit(msg.sender, address(0), msg.value,_prizePool[address(0)]  );
      }
 
 } 
