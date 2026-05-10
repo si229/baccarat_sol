ec85cc99a52de1b0a772ecc56d327110cf6377a8
c <se@c.com>
2026-05-10 12:42:17 +0800

    a

--- contracts/ERCPepe.sol
+++ contracts/ERCPepe.sol
@@ -5,9 +5,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockUSDT is ERC20 {
-    constructor() ERC20("Mock USDT", "USDT") {
-        _mint(msg.sender, 1_000_000 * 10 ** 6);
+contract PEPE is ERC20 {
+    constructor() ERC20("Mock PEPE", "PEPE") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 
     function mint(address to, uint amount) external {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,17 @@ async function main() {
   const Baccarat = await hre.ethers.getContractFactory("Baccarat");
   const baccarat = await Baccarat.deploy();
   await baccarat.waitForDeployment();
-  console.log("Baccarat deployed to:", baccarat.target);
+  console.log("baccarat deployed to:", baccarat.target);
+
+  const USDT = await hre.ethers.getContractFactory("MockUSDT");
+  const Usdt = await USDT.deploy();
+  await Usdt.waitForDeployment();
+  console.log("USDT deployed to:", Usdt.target);
+
+  const PEPE = await hre.ethers.getContractFactory("PEPE");
+  const Pepe = await PEPE.deploy();
+  await Pepe.waitForDeployment();
+  console.log("PEPE deployed to:", Pepe.target);
 }
 
 // 错误处理
